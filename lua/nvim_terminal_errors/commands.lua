6460d9e0c8210706f2a5b122a538e392f9e7e748
Julien Lecoq <julien_lecoq@protonmail.com>
2022-10-16 01:42:56 +0200

    Fix: unresolved symbol

--- lua/nvim_terminal_errors/commands.lua
+++ lua/nvim_terminal_errors/commands.lua
@@ -1,3 +1,4 @@
+local api = vim.api
 local angular = require('nvim_terminal_errors.errors_from.angular')
 local table_util = require('nvim_terminal_errors.table_util')
 local string_util = require('nvim_terminal_errors.string_util')
@@ -43,7 +44,7 @@ local function has_successful_statement(row, projectType)
 end
 
 local function is_successful_build()
-    local content = vim.api.nvim_buf_get_lines(buffer.current_buffer, buffer.first_line, buffer.last_line, false)
+    local content = api.nvim_buf_get_lines(buffer.current_buffer, buffer.first_line, buffer.last_line, false)
 
     for _, row in table_util.rpairs(content) do
         if has_failed_statement(row) then
@@ -59,7 +60,7 @@ local function is_successful_build()
 end
 
 local function get_build_last_failed_statement_row()
-    local content = vim.api.nvim_buf_get_lines(buffer.current_buffer, buffer.first_line, buffer.last_line, false)
+    local content = api.nvim_buf_get_lines(buffer.current_buffer, buffer.first_line, buffer.last_line, false)
 
     for index, row in table_util.rpairs(content) do
         if has_failed_statement(row) then
@@ -77,7 +78,7 @@ end
 local function go_to_previous_error_from_current_cursor_position(build_last_failed_statement_row)
     build_last_failed_statement_row = build_last_failed_statement_row or get_build_last_failed_statement_row()
     local lastLine = buffer.get_cursor_row_position() - 1
-    local content = vim.api.nvim_buf_get_lines(buffer.current_buffer, build_last_failed_statement_row, lastLine, false)
+    local content = api.nvim_buf_get_lines(buffer.current_buffer, build_last_failed_statement_row, lastLine, false)
 
     for _, row in table_util.rpairs(content) do
         if row_has_error(row) then
@@ -91,7 +92,7 @@ end
 
 local function go_to_previous_error_from_last_buffer_line(build_last_failed_statement_row)
     build_last_failed_statement_row = build_last_failed_statement_row or get_build_last_failed_statement_row()
-    local content = vim.api.nvim_buf_get_lines(buffer.current_buffer, build_last_failed_statement_row, buffer.last_line,
+    local content = api.nvim_buf_get_lines(buffer.current_buffer, build_last_failed_statement_row, buffer.last_line,
         false)
 
     for _, row in table_util.rpairs(content) do
@@ -129,7 +130,7 @@ local function go_to_first_error_from_last_failed_statement()
         return false
     end
 
-    local content = vim.api.nvim_buf_get_lines(buffer.current_buffer, failedStatementRowIndex, buffer.last_line,
+    local content = api.nvim_buf_get_lines(buffer.current_buffer, failedStatementRowIndex, buffer.last_line,
         false)
 
     for _, row in ipairs(content) do
@@ -143,7 +144,7 @@ local function go_to_first_error_from_last_failed_statement()
 end
 
 local function go_to_first_error_from_current_cursor_position()
-    local content = vim.api.nvim_buf_get_lines(0, buffer.get_cursor_row_position(), buffer.last_line, false)
+    local content = api.nvim_buf_get_lines(0, buffer.get_cursor_row_position(), buffer.last_line, false)
 
     for _, row in ipairs(content) do
         if row_has_error(row) then
